0b8552331798159969193f822bd4117ef4926bdb
CompulsiveCoder <compulsivecoder@gmail.com>
2016-04-25 16:54:22 +1000

    Added missing files

---
+++ src/ReplacerConsole/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace Replacer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Run(args);
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex);
+            }
+        }
+
+        public static void Run(string[] args)
+        {
+            if (args.Length != 4)
+                Help ();
+            else {
+                string workingDir = args [0];
+
+                string fileQuery = args [1];
+
+                string replaceFrom = args [2];
+
+                string replaceWith = args [3];
+
+                int number = 0;
+
+                var replacer = new Replacer (Path.GetFullPath(workingDir));
+                replacer.IsVerbose = true; // TODO: Allow user to set this via an argument
+                replacer.CommitChanges = true;
+
+                replacer.Replace (fileQuery, replaceFrom, replaceWith);
+
+                Console.WriteLine ("Replace complete.");
+                Console.WriteLine ("# files modified: " + number);
+            }
+        }
+
+        static public void ShowError(Exception ex)
+        {
+            Console.WriteLine("===== Error =====");
+            Console.WriteLine(ex.ToString());
+        }
+
+        static public void Help()
+        {
+            Console.WriteLine ("-------------------");
+            Console.WriteLine ("  replacer - Help  ");
+            Console.WriteLine ("-------------------");
+            Console.WriteLine ();
+            Console.WriteLine ("Syntax:");
+            Console.WriteLine ("  mono replacer.exe [path] [fileName] [oldValue] [newValue]");
+            Console.WriteLine ();
+            Console.WriteLine ("Example:");
+            Console.WriteLine ("  mono replacer.exe . file.txt \"oldValue\" \"newValue\"");
+            Console.WriteLine ();
+
+        }
+    }
+}
